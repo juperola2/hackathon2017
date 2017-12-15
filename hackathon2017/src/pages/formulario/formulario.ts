5f74d3df58083a49e126f683b83a49eca494760b
juperola2 <shih.marg@gmail.com>
2017-12-15 16:23:05 -0300

    formulario

---
+++ hackathon2017/src/pages/formulario/formulario.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the FormularioPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-formulario',
+  templateUrl: 'formulario.html',
+})
+export class FormularioPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad FormularioPage');
+  }
+
+  public proximo() {
+    console.log('redireciona');
+    console.log(document.querySelector('input').value);
+    //this.navCtrl.push(FormularioPage);
+  }
+
+}
